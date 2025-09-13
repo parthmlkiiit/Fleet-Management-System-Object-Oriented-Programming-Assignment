d1201ee041b49dc7c2f949ac7576a011ba3831bf
parth <parth24405@iiitd.ac.in>
2025-09-14 03:38:54 +0530

    first local sync

---
+++ Vehicle.java
@@ -0,0 +1,7 @@
+public class Vehicle {
+    public static void main(String[] args) {
+      double mydouble = 9.78d;
+      String mystring =  mydouble + "";
+        System.out.println(mystring);
+    }
+}
